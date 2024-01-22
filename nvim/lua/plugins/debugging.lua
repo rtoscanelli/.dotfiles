b1a7741a3dc3c110626ae6198c7ad301a06cd5b2
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-01-22 11:32:19 +0000

    Add debuger

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,36 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "leoluz/nvim-dap-go",
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup()
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        vim.keymap.set("n", "<Leader>db", function()
+            dap.toggle_breakpoint()
+        end)
+
+        vim.keymap.set("n", "<Leader>dc", function()
+            dap.continue()
+        end)
+
+        -- go debug dap
+        require("dap-go").setup()
+    end,
+}
