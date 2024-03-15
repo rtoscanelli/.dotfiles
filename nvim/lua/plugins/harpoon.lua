9061e8e1e1e0fd7aa718f698adc2b1d690a2283b
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-03-15 14:23:49 +0000

    Add more keybinds to harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -17,5 +17,10 @@ return {
         vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end)
         vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end)
         vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<A-5>", function() ui.nav_file(5) end)
+        vim.keymap.set("n", "<A-6>", function() ui.nav_file(6) end)
+        vim.keymap.set("n", "<A-7>", function() ui.nav_file(7) end)
+        vim.keymap.set("n", "<A-8>", function() ui.nav_file(8) end)
+        vim.keymap.set("n", "<A-9>", function() ui.nav_file(9) end)
     end
 }
