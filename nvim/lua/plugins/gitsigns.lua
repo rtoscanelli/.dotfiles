dd27215c58c4228abe290a0d5b77beb17728f469
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-03-31 19:38:32 +0100

    Add toggle blame keybind

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,16 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require('gitsigns').setup {
+        require("gitsigns").setup({
             signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            }
-        }
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        })
+        vim.keymap.set("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true })
     end,
 }
