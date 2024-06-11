35093fd78cc3e9d65465ab62b4a1538f42de4ba6
Ricardo Toscanelli <ricardotoscanelli@gmail.com>
2024-06-11 16:56:46 +0100

    Fix: dart indent spaces

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,6 +1,12 @@
 return {
-    "terrortylor/nvim-comment",
-    config = function()
-        require('nvim_comment').setup()
-    end,
+	"terrortylor/nvim-comment",
+	config = function()
+		require("nvim_comment").setup({
+			hook = function()
+				if vim.api.nvim_buf_get_option(0, "filetype") == "dart" then
+					vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+				end
+			end,
+		})
+	end,
 }
